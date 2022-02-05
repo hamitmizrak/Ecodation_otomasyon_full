319063300b9b7e70d4e9636a4f5bccae53bc403d
hamitmizrak <hamitmizrak@gmail.com>
2022-02-05 10:10:28 +0300

    IDaoConnection ==> interface

---
+++ src/com/ecodation/dao/IDaoConnection.java
@@ -0,0 +1,24 @@
+package com.ecodation.dao;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import com.ecodation.database.DatabaseConnection;
+
+public interface IDaoConnection<T> {
+
+	// CRUD
+	public void create(T t);
+
+	public void update(T t);
+
+	public void delete(T t);
+
+	public ArrayList<T> list();
+
+	// govdesiz metot
+	default Connection getInterfaceConnection() {
+		return DatabaseConnection.getInstance().getConnection();
+	}
+
+}
