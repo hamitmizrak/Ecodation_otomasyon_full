197337ca692261faa97fec7421fb9379fcd4b3e0
hamitmizrak <hamitmizrak@gmail.com>
2022-02-05 10:22:40 +0300

    Sql Table

--- src/com/ecodation/database/AllSqlCodes.sql
+++ src/com/ecodation/database/AllSqlCodes.sql
@@ -1,3 +1,11 @@
 CREATE SCHEMA `ecodation_2_otomasyon` DEFAULT CHARACTER SET utf8 COLLATE utf8_turkish_ci ;
 
 use ecodation_2_otomasyon;
+
+
+CREATE TABLE `ecodation_2_otomasyon`.`admin` (
+  `admin_id` INT NOT NULL AUTO_INCREMENT,
+  `admin_name` VARCHAR(450) NULL,
+  `admin_surname` VARCHAR(450) NULL,
+  `created_date` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`admin_id`));
